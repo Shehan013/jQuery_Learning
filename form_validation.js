a177647ebc69b619cfb836bb73d355f803718618
shehan013 <shehanmaleesha13@gmail.com>
2024-09-07 08:50:21 +0530

    form_validationS

---
+++ form_validation.js
@@ -0,0 +1,43 @@
+$(function(){
+    $("form[name='registration']").validate({
+        rules: {
+
+            name : "required",
+
+            email :{
+                required: true,
+                email: true
+            },
+
+            password : {
+                required: true,
+                minlength: 5
+            },
+
+            confirmpassword : {
+                required: true,
+                minlength:5,
+                equalTo : "#password"
+            }
+        },
+
+        messages: {
+            name: "Please enter your name",
+        
+            email : {
+                required: "Please enter your email",
+                email : "Please enter a valid email"
+            },
+        
+            password : {
+                required: "Please enter a password",
+                minlength: "Your password must be at least 5 characters long"
+            },
+            confirmpassword: {
+                required: "Please re-enter your password",
+                minlength: "Your password must be at least 5 characters long",
+                equalTo : "Please enter the same password as above"
+            }
+        }
+    });
+})
